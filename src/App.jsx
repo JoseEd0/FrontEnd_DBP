b9f8b3dc234dfd96c52faaa2506b2252616c143a
dillescas01 <diego.illescas@utec.edu.pe>
2024-07-04 11:04:01 -0500

    Avance-Home

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,9 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Login from './components/login';
 import Register from './components/Register';
+import Home from './components/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
-
-const Home = () => {
-  <div>
-    <h2>Welcome to the Course App!</h2>
-    <p>Please use the menu to navigete through the application.</p>
-  </div>
-};
-
 
 function App(){
   return (
@@ -25,7 +18,7 @@ function App(){
         </Routes>
       </Router>
     </AuthProvider>
-  )
+  );
 }
 
 export default App;

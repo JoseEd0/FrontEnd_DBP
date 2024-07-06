7ba84fb437b8b135aaa60bb075fe1391e2e368fd
dillescas01 <diego.illescas@utec.edu.pe>
2024-07-06 17:28:44 -0500

    home_after_login_register

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import './App.css';
 import NavBar from './components/NavBar';
 import Login from './components/login';
@@ -5,16 +6,17 @@ import Register from './components/Register';
 import Home from './components/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
-function App(){
+function App() {
   return (
     <AuthProvider>
       <Router>
         <NavBar />
         <Routes>
-          <Route path= "/" element={<Home />} />
-          <Route path= "/login" element={<Login />} />
-          <Route path= "/register" element={<Register />} />
+          <Route path="/" element={<ProtectedRoute element={<Home />} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
     </AuthProvider>
